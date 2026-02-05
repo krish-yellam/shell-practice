3fbc778e5726384f99867da183ed7cea2d44ae06
Krish <3dstudent1997@gmail.com>
2026-02-05 12:58:00 +0530

    third

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-echo "Suresh:: Hello Ramesh, How are you?"
-echo "Ramesh:: Hi Suresh, I am fine, How are you?"
-echo "Suresh:: Doing fine. what are you learning"
-echo "Ramesh:: I am learning Shell. what about you"
+PERSON1=Suresh
+PERSON2=Ramesh
+
+
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine, How are you?"
+echo "$PERSON1:: Doing fine. what are you learning $PERSON2"
+echo "$PERSON2:: I am learning Shell. what about you $PERSON1"
 
