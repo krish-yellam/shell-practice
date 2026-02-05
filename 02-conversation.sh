79ac2da587b503954247b1de39a37db337796463
Krish <3dstudent1997@gmail.com>
2026-02-05 12:40:42 +0530

    new one

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Suresh:: Hello Ramesh, How are you?"
+echo "Ramesh:: Hi Suresh, I am fine, How are you?"
+echo "Suresh:: Doing fine. what are you learning"
+echo "Ramesh:: I am learning Shell. what about you"
+
