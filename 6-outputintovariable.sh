a08eefee7b3c249a6be0dba45ebe8157fb1c8459
Krish <3dstudent1997@gmail.com>
2026-02-05 13:37:39 +0530

    third

--- 6-outputintovariable.sh
+++ 6-outputintovariable.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-TIMESTAMP=$(date)
+START-TIME=$(date +%s) 
 
-echo "Script executed at :$TIMESTAMP"
+echo "Script executed at :$START-TIME"
 
+sleep 10
+
+END-TIME=$(date +%s)
+
+TOTAL_TIME=(($START-TIME-$END-TIME))
+
+echo "total time :$TOTAL_TIME"
