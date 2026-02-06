9fe05e4669e0eabcb7f7eb35660d3bdbd57bd012
Krish <3dstudent1997@gmail.com>
2026-02-06 17:59:29 +0530

    checking

---
+++ 11-funcations.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root user access"
+    exit 1
+fi
+
+VALIDATE(){
+   if [ $1 -ne 0 ]; then
+        echo " $2 is ....FAILURE"
+    else 
+        echo "$2 is Sucess"
+}
+
+    dnf install nginx -y
+    VALIDATE $? Installing Nginx
+
+
+    dnf install mysql -y
+    VALIDATE $? Installing Mysql
+
+    dnf install nodejs -y
+    VALIDATE $? Installing Nodejs
