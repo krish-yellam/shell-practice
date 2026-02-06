6c70ec69d93624d855e313bc4e557135e2ae41f1
Krish <3dstudent1997@gmail.com>
2026-02-06 20:33:34 +0530

    checking

--- 13-logs.sh
+++ 13-logs.sh
@@ -1,32 +1,32 @@
-#!/bin/bash
+    #!/bin/bash
 
-USERID=$(id -u)
-LOGS_FOLDER="/var/log/shell-script"
-LOGS_FILES="/var/log/shell-script/$0.log"
+    USERID=$(id -u)
+    LOGS_FOLDER="/var/log/shell-script"
+    LOGS_FILES="/var/log/shell-script/$0.log"
 
 
-if [ $USERID -ne 0 ]; then
-    echo "Try with root user"
-    exit 1
-fi
+    if [ $USERID -ne 0 ]; then
+        echo "Try with root user" | tee $LOGS_FILES
+        exit 1
+    fi
 
-mkdir -p $LOGS_FOLDER
+    mkdir -p $LOGS_FOLDER
 
-VALIDATE(){
-if [ $1 -ne 0 ]; then
-    echo " $2 is failure"
-    exit 1
-else 
-    echo " $2 is success"
-fi
+    VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo " $2 is failure" | tee $LOGS_FILES
+        exit 1
+    else 
+        echo " $2 is success" | tee $LOGS_FILES
+    fi
 
-}
+    }
 
-dnf install nginx -y &>> $LOGS_FILES
-VALIDATE $? Installing Nginx
+    dnf install nginx -y &>> $LOGS_FILES
+    VALIDATE $? Installing Nginx
 
-dnf install mysql -y &>> $LOGS_FILES
-VALIDATE $? Installing MYSQL
+    dnf install mysql -y &>> $LOGS_FILES
+    VALIDATE $? Installing MYSQL
 
-dnf install nodejs -y &>> $LOGS_FILES
-VALIDATE $? Installing Nodejs
+    dnf install nodejs -y &>> $LOGS_FILES
+    VALIDATE $? Installing Nodejs
