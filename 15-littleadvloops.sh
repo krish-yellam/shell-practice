97d7e21fa34b673dacfa37a3992fa42c5cd2d63d
Krish <3dstudent1997@gmail.com>
2026-02-08 15:32:50 +0530

    loops

---
+++ 15-littleadvloops.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/logs/shell-script"
+LOG_FILES="/var/logs/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "please run this script with rootuser access"
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+if [ $1 -ne 0 ] then
+    echo " $2 ..... FAILURE" | tee $LOG_FILES
+    exit 1
+else 
+    echo "$2 ......SUCCESS" | tee $LOGS_FILES
+fi
+}
+
+for package in $@
+
+do 
+    dnf install $package -y
+    VALIDATE $? "$package installaion"
+done
