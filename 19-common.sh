c81a0c6808cbaf7b15997c886510a51c5f29214d
Krish <3dstudent1997@gmail.com>
2026-02-27 14:54:47 +0530

    new code

--- 19-common.sh
+++ 19-common.sh
@@ -8,7 +8,7 @@ echo "PID of 19-common- is $$"
 
 
 #sh 20-newcommon.sh
-source ./20-newcommon.sh
-echo "PID of 20-newcommon.sh is $$"
+#source ./20-newcommon.sh
+#echo "PID of 20-newcommon.sh is $$"
 
 sh 20-newcommon.sh
