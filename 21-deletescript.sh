3734b7eb792437bcdf023dcf3a283b9d4c2e6d23
Krish <3dstudent1997@gmail.com>
2026-03-03 13:17:33 +0530

    new script

---
+++ 21-deletescript.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+LOGS_DIR=/home/ec2-user/app-logs
+LOGS_FILE="$LOGS_DIR/$0.log"
+
+if [ ! -d $LOGS_DIR ]; then
+    echo "$LOGS_DIR does not exist"
+    exit 1
+fi
+
+FILES_TO_DEL=$(find $LOGS_DIR -name "*.log" -type f -mtime +14) 
+    echo "$FILES_TO_DEL"
