abf89a4d04d9a6b699e3cdf4741244a32c4bcec5
Krish <3dstudent1997@gmail.com>
2026-03-04 13:05:37 +0530

    new update

--- 21-deletescript.sh
+++ 21-deletescript.sh
@@ -4,17 +4,10 @@ LOGS_DIR=/home/ec2-user/app-logs
 LOGS_FILE="$LOGS_DIR/$0.log"
 
 if [ ! -d $LOGS_DIR ]; then
-    echo "$LOGS_DIR does not exist"
+    echo "Directory does not exist"
     exit 1
 fi
 
-FILES_TO_DEL=$(find $LOGS_DIR -name "*.log" -type f -mtime +14) 
-    # echo "$FILES_TO_DEL"
+FILES_TO_DEL=$(find $LOGS_DIR -name "*.log" -type f -mtime +14)
+    echo "files to delete $FILES_TO_DEL" 
 
-while IFS= read -r filepath
-    do
-        # Process the current line (stored in the variable $LINE)
-        echo "Deleteing the $filepath"
-        rm -f $filepath
-        echo "Deleted the $filepath"
-    done <<< $FILES_TO_DEL
