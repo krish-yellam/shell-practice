630ee65da12e1ce45965e2d064664479c52e2567
Krish <3dstudent1997@gmail.com>
2026-03-17 13:15:13 +0530

    disk usage

--- 24-diskusage.sh
+++ 24-diskusage.sh
@@ -9,13 +9,13 @@ logs(){
     echo "$(date "+%Y-%m-%d %H:%M:%S") | $1" | tee $LOGS_FILE
 }
 
-DISK_USAGE=$(df -t | grep -v filesystem)
+DISK_USAGE=$(df -hT | grep -v filesystem)
 USAGE_THRESHOLD=3
 
 while IFS= read -r line
     do
-        USAGE=$$($line | awk '{print $6}' | cut -d "%" -f1)
-        PARTITION=$($line | awk '{print $7}')
+        USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
+        PARTITION=$(echo $line | awk '{print $7}')
         
         if [ "$USAGE" -ge"$USAGE_THRESHOLD" ]; then
             MESSAGE+="High disk usage on $PARTITON:$USAGE"
